9a97a242ad4a07f84053db432c9156097a895710
roooold <deleonharold0209@gmail.com>
2023-07-04 14:17:53 +0800

    announcement ui

--- Admin/Mindmatters/src/pages/AnnouncementPage/Annoucement.jsx
+++ Admin/Mindmatters/src/pages/AnnouncementPage/Annoucement.jsx
@@ -9,9 +9,27 @@ const Annoucement = () => {
             <Sidebar/>
             <div className="announceContainer">
                 <Navbar/>
-                <p>Announce</p>
+    <h2>Announcement Form</h2><br></br>
+    <form id="announcementForm">
+      <div class="form-group">
+        <label for="title">Title:</label>
+        <input type="text" id="title" name="title" required/>
+      </div>
+      <div class="form-group">
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>
+      </div>
+      <div class="form-group">
+        <label for="picture">Picture:</label>
+        <input type="file" id="picture" name="picture"/>
+      </div>
+      <div class="form-group">
+        <input type="submit" value="Submit"/>
+      </div>
+    </form>
+  </div>
             </div>
-   </div>
+   
   )
 }
 
