746704acc5cab9714ae2f81ac76d5fff719b8021
RACoderPH <angelesronmel0376@gmail.com>
2023-10-07 23:07:34 +0800

    Updated Backend with mailer

--- Backend/Routes/ForgotRoute/forgot.js
+++ Backend/Routes/ForgotRoute/forgot.js
@@ -1,14 +1,52 @@
 const express = require("express");
 const router = express.Router();
-
+const nodemailer = require('nodemailer');
 const db = require('../database'); 
+
+
+function generateVerificationToken() {
+  // Generate a random token
+  const token = Math.random().toString(36).substr(2);
+  return token;
+}
+
+// Function to send a verification email
+function sendVerificationEmail(email, verificationLink) {
+  // Create a transporter for sending emails (replace with your email service provider details)
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'mindmattersstdominic@gmail.com',
+      pass: 'swutnhnnzxmdjytp',
+    },
+  });
+
+  // Setup email data
+  const mailOptions = {
+    from: 'mindmattersstdominic@gmail.com',
+    to: email,
+    subject: 'Forgot Password',
+    html: `<p>Don't Share this Code</p>
+           <a href="${verificationLink}">${verificationLink}</a>`,
+  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Failed to send verification email:', error);
+    } else {
+      console.log('Verification email sent:', info.response);
+    }
+  });
+}
+
 
 
 router.post('/forgot', (req, res) => {
     const email = req.body.email; // Assuming email is sent in the request body
-  
+    const verificationToken = generateVerificationToken();
     const checkEmailQuery = 'SELECT * FROM `user_info` WHERE Email = ?';
-  
+
     db.query(checkEmailQuery, [email], (error, results) => {
       if (error) {
         console.error('Failed to fetch data:', error);
@@ -17,6 +55,8 @@ router.post('/forgot', (req, res) => {
         if (results.length > 0) {
           // If there are results, the email exists
           res.send({ message: 'exists' });
+          const verificationLink = `This is your Forgot Password OTP:${verificationToken}`; // Replace with your verification link
+          sendVerificationEmail(email, verificationLink);
         } else {
           // If there are no results, the email does not exist
           res.send({ message: 'not exists' });
