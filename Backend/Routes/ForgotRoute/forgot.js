c74801f46198b8cb7c86eedbe3fa1f71ddbf0278
RACoderPH <angelesronmel0376@gmail.com>
2023-10-07 22:48:11 +0800

    Updated Backend

--- Backend/Routes/ForgotRoute/forgot.js
+++ Backend/Routes/ForgotRoute/forgot.js
@@ -5,32 +5,25 @@ const db = require('../database');
 
 
 router.post('/forgot', (req, res) => {
-  const email = (req.body.email || '').trim().toLowerCase(); // Trim and convert to lowercase
-
-  if (!email) {
-    // Handle the case where the email is missing or empty
-    return res.status(400).json({ error: 'Email is missing or empty' });
-  }
-
-  const checkEmailQuery = 'SELECT * FROM `user_info` WHERE LOWER(Email) = ?'; // Use LOWER for case-insensitive comparison
-
-  db.query(checkEmailQuery, [email], (error, results) => {
-    if (error) {
-      console.error('Failed to fetch data:', error);
-      res.status(500).json({ error: 'Failed to fetch data' });
-    } else {
-      if (results.length > 0) {
-        // If there are results, the email exists
-        res.json({ message: 'exists' });
+    const email = req.body.email; // Assuming email is sent in the request body
+  
+    const checkEmailQuery = 'SELECT * FROM `user_info` WHERE Email = ?';
+  
+    db.query(checkEmailQuery, [email], (error, results) => {
+      if (error) {
+        console.error('Failed to fetch data:', error);
+        res.status(500).json({ error: 'Failed to fetch data' });
       } else {
-        // If there are no results, the email does not exist
-        res.json({ message: 'not exists' });
+        if (results.length > 0) {
+          // If there are results, the email exists
+          res.message({ message: 'exists' });
+        } else {
+          // If there are no results, the email does not exist
+          res.message({ message: 'not exists' });
+        }
       }
-    }
+    });
   });
-});
-
-
   
 
 module.exports = router;
