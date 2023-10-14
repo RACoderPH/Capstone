7f4e0046a7b48bcda18cf0323d1b322c91b47bc9
cjdev19 <134400871+cjdev19@users.noreply.github.com>
2023-10-14 22:04:48 +0800

    Updated Dashboard

--- Admin/Mindmatters/src/components/Chart/Chart.jsx
+++ Admin/Mindmatters/src/components/Chart/Chart.jsx
@@ -1,54 +1,85 @@
-import './chart.scss'
-
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-
-const data = [
-  {
-    name: 'Depression',
-    uv: 50,
- 
-  },
-  {
-    name: 'Anxiety',
-    uv: 60,
-  
-  },
-  {
-    name: 'Stress',
-    uv: 10,
-    
-  },
-  {
-    name: 'Normal',
-    uv: 20,
-    
-  },
-];
-
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import './chart.scss';
+import { BarChart, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Legend, Bar, PieChart, Pie } from 'recharts';
 
 const Chart = () => {
-  return (
+  const [averages, setAverages] = useState({});
+  const [assessmentStatus, setAssessmentStatus] = useState({});
+
+  useEffect(() => {
+    axios.get('https://mindmatters-ejmd.onrender.com/count')
+      .then(response => {
+        setAssessmentStatus(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching assessment status: ' + error);
+      });
+
+    axios.get('https://mindmatters-ejmd.onrender.com/student_result')
+      .then(response => {
+        setAverages(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ' + error);
+      });
+  }, []);
   
+
+  const data1 = [
+    { name: "Already Taken", users: parseInt(assessmentStatus.taken) || 0 },
+    { name: "Not yet Taken", users: parseInt(assessmentStatus.notTaken) || 0 },
+  ];
+  
+  
+
+  return (
     <ResponsiveContainer width="100%" aspect={3}>
-    <AreaChart
-      width={500}
-      height={400}
-      data={data}
-      margin={{
-        top: 10,
-        right: 30,
-        left: 0,
-        bottom: 0,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-    </AreaChart>
-  </ResponsiveContainer>
-  )
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ flex: 1 }}>
+          <BarChart
+            width={500}
+            height={300}
+            data={[
+              { name: 'Stress', Student: averages.avg_stress },
+              { name: 'Anxiety', Student: averages.avg_anxiety },
+              { name: 'Depression', Student: averages.avg_depression },
+            ]}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 80,
+              bottom: 5,
+            }}
+            barSize={30}
+          >
+            <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Bar dataKey="Student" fill="#8884d8" background={{ fill: '#eee' }} />
+          </BarChart>
+        </div>
+
+        <div style={{ flex: 1 }}>
+          <PieChart width={400} height={400}>
+            <Pie
+              dataKey="users" // Data key should match the property name in data1
+              isAnimationActive={false}
+              data={data1}
+              cx={200}
+              cy={200}
+              outerRadius={80}
+              fill="#8884d8"
+              label
+            />
+            <Tooltip />
+          </PieChart>
+        </div>
+      </div>
+    </ResponsiveContainer>
+  );
 }
 
-export default Chart
+export default Chart;
